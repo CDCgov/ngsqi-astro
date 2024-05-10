4e84738e8b8b2a1cb41cae80692283ef2f08a36c
fay0 <fay0@cdc.gov>
2024-05-10 15:24:28 -0400

    updated with neat submission script

--- sge_submit.sh
+++ sge_submit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 source /etc/profile
 
-#$ -N test_dwgsim_workflow
+#$ -N test_NEAT_workflow
 #$ -cwd
-#$ -l mem_free=62G
+#$ -l mem_free=32G
 
 module load nextflow/23
 module load singularity
 
 nextflow run main.nf -profile singularity \
--entry DWGSIM \
---input /scicomp/groups-pure/Projects/CSELS_NGSQI_insillico/dwgsim_test_samplesheet.csv \
+-entry NEAT \
+--input /scicomp/groups-pure/Projects/CSELS_NGSQI_insillico/amr-metagenomics/p_aeruginosa_goldstandard_samplesheet.csv \
 --outdir output
